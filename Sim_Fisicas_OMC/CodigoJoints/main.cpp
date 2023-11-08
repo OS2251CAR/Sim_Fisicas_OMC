b698ac2ef2a57a48d7cb8fd4555a77a23e340da1
OS2251CAR <109360654+OS2251CAR@users.noreply.github.com>
2023-11-08 12:35:28 -0600

    avance

--- Sim_Fisicas_OMC/CodigoJoints/main.cpp
+++ Sim_Fisicas_OMC/CodigoJoints/main.cpp
@@ -5,11 +5,10 @@ constexpr float SCALE = 30.0f;
 
 class Cube {
 public:
-    Cube(b2World &world, float x, float y, int state = 0) {
+    Cube(b2World &world, float x, float y, int dynamic = 0,float density = 1.0f) {
         b2BodyDef bodydef;
         bodydef.position.Set(x / SCALE, y / SCALE);
-        switch (state)
-        {
+        switch (dynamic) {
             case 0:
                 bodydef.type = b2_staticBody;
                 break;
@@ -20,7 +19,6 @@ public:
                 bodydef.type = b2_kinematicBody;
                 break;
         }
-
         body = world.CreateBody(&bodydef);
 
         b2PolygonShape shape;
@@ -28,7 +26,7 @@ public:
 
         b2FixtureDef fixturedef;
         fixturedef.shape = &shape;
-        fixturedef.density = 1.0f;
+        fixturedef.density = density;
         body->CreateFixture(&fixturedef);
 
         box.setSize(sf::Vector2f(10.f, 10.f));
@@ -63,76 +61,79 @@ private:
 };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1000, 900), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Space Invaders v0.0.1!");
 
     b2Vec2 gravity(0.0f, 0.0f);
     b2World world(gravity);
 
-    Cube cube(world, 400, 300, 1);
-    Cube cube2(world, 600, 300, 1);
+    Cube cube(world, 400, 300,1);
 
-    Cube cube3(world, 400, 500, 1);
-    Cube cube4(world, 400, 500, 0);
+    Cube cube2(world, 500, 300, 1);
 
-    Cube cube5(world, 100, 600, 1);
-    Cube cube6(world, 100, 600, 0);
+    Cube cube3(world, 600, 400, 1,100000.f);
 
-    Cube cube7(world, 800, 400, 0);
+    b2PrismaticJointDef jointDef;
+    jointDef.Initialize(cube.getBody(), cube3.getBody(), cube3.getBody()->GetWorldCenter(), b2Vec2(1.0f, 0.0f));
+    jointDef.lowerTranslation = -10.0f;
+    jointDef.upperTranslation = 10.0f;
+    jointDef.enableLimit = true;
 
-    Cube cube8(world, 700, 400, 1);
-
-
-
-    b2WheelJointDef jointDefW;
-    jointDefW.Initialize(cube7.getBody(),cube8.getBody(), cube7.getBody()->GetWorldCenter(), b2Vec2(1.0f, 0.0f));
-
-    b2PrismaticJointDef jointDefPr;
-    jointDefPr.Initialize(cube5.getBody(), cube6.getBody(), cube5.getBody()->GetWorldCenter(), b2Vec2(1.0f, 0.0f));
-    jointDefPr.lowerTranslation = -10.0f;
-    jointDefPr.upperTranslation =10.0f;
-    jointDefPr.enableLimit = true;
-    world.CreateJoint(&jointDefPr);
-
-    b2RevoluteJointDef jointDefR;
-    jointDefR.Initialize(cube3.getBody(), cube4.getBody(), cube4.getBody()->GetWorldCenter());
-
-    float speed = 2.0f;
-    float torque = 1000.0f;
-    jointDefR.enableMotor = true;
-    jointDefR.maxMotorTorque = torque;
-    jointDefR.motorSpeed = speed;
-    world.CreateJoint(&jointDefR);
+    b2PrismaticJointDef jointDef2;
+    jointDef2.Initialize(cube2.getBody(), cube3.getBody(), cube3.getBody()->GetWorldCenter(), b2Vec2(0.0f, 1.0f));
+    jointDef2.lowerTranslation = -10.0f;
+    jointDef2.upperTranslation = 10.0f;
+    jointDef2.enableLimit = true;
 
 
+    Cube cube4(world, 650, 450, 1);
 
-    b2DistanceJointDef jointDef;
-    jointDef.Initialize(cube.getBody(), cube2.getBody(), cube.getBody()->GetWorldCenter(), cube2.getBody()->GetWorldCenter());
+    b2RevoluteJointDef jointDef3;
 
-    float frequencyHz = 30.0f;
-    float dampingRatio = 0.1f;
+    jointDef3.Initialize(cube4.getBody(), cube3.getBody(), cube3.getBody()->GetWorldCenter());
+    jointDef3.lowerAngle = -10.0f * b2_pi / 180.0f;
+    jointDef3.upperAngle = 10.0f * b2_pi / 180.0f;
+    jointDef3.enableLimit = false;
+    jointDef3.enableMotor = false;
+    jointDef3.maxMotorTorque = 1.0f;
+    jointDef3.motorSpeed = 1.0f;
 
-    b2LinearStiffness(jointDef.stiffness, jointDef.damping, frequencyHz, dampingRatio, jointDef.bodyA, jointDef.bodyB);
 
-    world.CreateJoint( &jointDef);
+    b2PrismaticJoint* joint = (b2PrismaticJoint*)world.CreateJoint(&jointDef);
+    b2PrismaticJoint* joint2 = (b2PrismaticJoint*)world.CreateJoint(&jointDef2);
+    b2RevoluteJoint* joint3 = (b2RevoluteJoint*)world.CreateJoint(&jointDef3);
+
+    b2GearJointDef gearJointDef;
+
+    gearJointDef.bodyA = cube.getBody();
+    gearJointDef.bodyB = cube2.getBody();
+    gearJointDef.joint1 = joint;
+    gearJointDef.joint2 = joint2;
+    gearJointDef.ratio = 0.2f;
+
+    world.CreateJoint(&gearJointDef);
 
 
     while (window.isOpen()) {
-        sf::Event event;
+        sf::Event event{};
         while (window.pollEvent(event)) {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(0.1f, 0.0f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(0.1f, 0.0f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(-0.1f, 0.0f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(-0.1f, 0.0f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(0.0f, -0.1f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(0.0f, -0.1f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(0.0f, 0.1f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(0.0f, 0.1f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
@@ -141,12 +142,11 @@ int main() {
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
                 cube.stop();
+                cube2.stop();
             }
 
-            // The Z key event to close the window
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
                 window.close();
-                return 0; // return from the main function, effectively ending the program
             }
         }
 
@@ -155,20 +155,12 @@ int main() {
         cube2.update();
         cube3.update();
         cube4.update();
-        cube5.update();
-        cube6.update();
-        cube7.update();
-        cube8.update();
 
         window.clear();
         cube.draw(window);
         cube2.draw(window);
         cube3.draw(window);
         cube4.draw(window);
-        cube5.draw(window);
-        cube7.draw(window);
-        cube8.draw(window);
-
         window.display();
     }
     return 0;
--- Sim_Fisicas_OMC/sfmlPrueba/main.cpp
+++ Sim_Fisicas_OMC/sfmlPrueba/main.cpp
@@ -5,10 +5,20 @@ constexpr float SCALE = 30.0f;
 
 class Cube {
 public:
-    Cube(b2World &world, float x, float y) {
+    Cube(b2World &world, float x, float y, int dynamic = 0,float density = 1.0f) {
         b2BodyDef bodydef;
         bodydef.position.Set(x / SCALE, y / SCALE);
-        bodydef.type = b2_dynamicBody;
+        switch (dynamic) {
+            case 0:
+                bodydef.type = b2_staticBody;
+                break;
+            case 1:
+                bodydef.type = b2_dynamicBody;
+                break;
+            case 2:
+                bodydef.type = b2_kinematicBody;
+                break;
+        }
         body = world.CreateBody(&bodydef);
 
         b2PolygonShape shape;
@@ -16,7 +26,7 @@ public:
 
         b2FixtureDef fixturedef;
         fixturedef.shape = &shape;
-        fixturedef.density = 1.0f;
+        fixturedef.density = density;
         body->CreateFixture(&fixturedef);
 
         box.setSize(sf::Vector2f(10.f, 10.f));
@@ -51,40 +61,79 @@ private:
 };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1000, 900), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Space Invaders v0.0.1!");
 
     b2Vec2 gravity(0.0f, 0.0f);
     b2World world(gravity);
 
-    Cube cube(world, 400, 300);
-    Cube cube2(world, 400, 300);
+    Cube cube(world, 400, 300,1);
 
-    b2DistanceJointDef jointDef;
-    jointDef.Initialize(cube.getBody(), cube2.getBody(), cube.getBody()->GetWorldCenter(), cube2.getBody()->GetWorldCenter());
+    Cube cube2(world, 500, 300, 1);
 
-    float frequencyHz = 30.0f;
-    float dampingRatio = 0.1f;
+    Cube cube3(world, 600, 400, 1,100000.f);
 
-    b2LinearStiffness(jointDef.stiffness, jointDef.damping, frequencyHz, dampingRatio, jointDef.bodyA, jointDef.bodyB);
+    b2PrismaticJointDef jointDef;
+    jointDef.Initialize(cube.getBody(), cube3.getBody(), cube3.getBody()->GetWorldCenter(), b2Vec2(1.0f, 0.0f));
+    jointDef.lowerTranslation = -10.0f;
+    jointDef.upperTranslation = 10.0f;
+    jointDef.enableLimit = true;
+
+    b2PrismaticJointDef jointDef2;
+    jointDef2.Initialize(cube2.getBody(), cube3.getBody(), cube3.getBody()->GetWorldCenter(), b2Vec2(0.0f, 1.0f));
+    jointDef2.lowerTranslation = -10.0f;
+    jointDef2.upperTranslation = 10.0f;
+    jointDef2.enableLimit = true;
+
+
+    Cube cube4(world, 650, 450, 1);
+
+    b2RevoluteJointDef jointDef3;
+
+    jointDef3.Initialize(cube4.getBody(), cube3.getBody(), cube3.getBody()->GetWorldCenter());
+    jointDef3.lowerAngle = -10.0f * b2_pi / 180.0f;
+    jointDef3.upperAngle = 10.0f * b2_pi / 180.0f;
+    jointDef3.enableLimit = false;
+    jointDef3.enableMotor = false;
+    jointDef3.maxMotorTorque = 1.0f;
+    jointDef3.motorSpeed = 1.0f;
+
+
+    b2PrismaticJoint* joint = (b2PrismaticJoint*)world.CreateJoint(&jointDef);
+    b2PrismaticJoint* joint2 = (b2PrismaticJoint*)world.CreateJoint(&jointDef2);
+    b2RevoluteJoint* joint3 = (b2RevoluteJoint*)world.CreateJoint(&jointDef3);
+
+    b2GearJointDef gearJointDef;
+
+    gearJointDef.bodyA = cube.getBody();
+    gearJointDef.bodyB = cube2.getBody();
+    gearJointDef.joint1 = joint;
+    gearJointDef.joint2 = joint2;
+    gearJointDef.ratio = 0.2f;
+
+    world.CreateJoint(&gearJointDef);
 
 
     while (window.isOpen()) {
-        sf::Event event;
+        sf::Event event{};
         while (window.pollEvent(event)) {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(0.1f, 0.0f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(0.1f, 0.0f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(-0.1f, 0.0f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(-0.1f, 0.0f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(0.0f, -0.1f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(0.0f, -0.1f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
                 cube.getBody()->ApplyForceToCenter(b2Vec2(0.0f, 0.1f), true);
+                cube2.getBody()->ApplyForceToCenter(b2Vec2(0.0f, 0.1f), true);
             }
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
@@ -93,20 +142,25 @@ int main() {
 
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
                 cube.stop();
+                cube2.stop();
             }
 
-            // The Z key event to close the window
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
                 window.close();
-                return 0; // return from the main function, effectively ending the program
             }
         }
 
         world.Step(1 / 60.f, 8, 3);
         cube.update();
+        cube2.update();
+        cube3.update();
+        cube4.update();
 
         window.clear();
         cube.draw(window);
+        cube2.draw(window);
+        cube3.draw(window);
+        cube4.draw(window);
         window.display();
     }
     return 0;
